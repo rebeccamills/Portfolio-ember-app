7ff7d1a5b3529d783c83497ffc086496b15615f8
Rebecca Mills <vena.cava@gmail.com>
2015-02-13 14:59:23 -0500

    committing changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.route("portfolio");
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+ 
 });
